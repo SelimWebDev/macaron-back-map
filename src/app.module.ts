2ba9be453cc6a380e0dacdf32e922da5da98757a
SelimWebDev <selim.elmahi@gmail.com>
2022-01-18 13:39:30 +0100

    feat(reqGetAllArrond):implémentation de la gestion de la req Get sur /arrondissements qui retourne tout les arrondissements

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ArrondissementModule } from './arrondissement/arrondissement.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [ArrondissementModule,
+    MongooseModule.forRoot(
+      'mongodb+srv://masterp6:macaron2022@cluster0.ck76z.mongodb.net/mapData?retryWrites=true&w=majority',
+    )],
   controllers: [AppController],
   providers: [AppService],
 })
